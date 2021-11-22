4e2bba54c734a72ac1a69ffcc369a1531be83db8
Joey Davidson <j.davidson18@gmail.com>
2021-11-22 09:56:56 -0700

    cart total display created, rendered

--- client/src/Checkout/Checkout.js
+++ client/src/Checkout/Checkout.js
@@ -8,31 +8,38 @@ function Checkout() {
   console.log(basket);
   return (
     <div className="checkout">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.JPG"
-        alt=""
-        className="checkout__ad"
-      />
-      {basket?.length === 0 ? (
-        <div>
-          <h2>Your shopping cart is empty</h2>
-        </div>
-      ) : (
-        <div>
-          <h2>Your Shopping Basket</h2>
-          {basket.map((product) => {
-            return (
-              <CheckoutProduct
-                item={product.id}
-                title={product.title}
-                image={product.image}
-                price={product.price}
-                rating={product.rating}
-              />
-            );
-          })}
-        </div>
-      )}
+      <div className="checkout__products">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.JPG"
+          alt=""
+          className="checkout__ad"
+        />
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your shopping cart is empty</h2>
+          </div>
+        ) : (
+          <div>
+            <h2>Your Shopping Basket</h2>
+            {basket.map((product) => {
+              return (
+                <CheckoutProduct
+                  item={product.id}
+                  title={product.title}
+                  image={product.image}
+                  price={product.price}
+                  rating={product.rating}
+                />
+              );
+            })}
+          </div>
+        )}
+      </div>
+      <div className="checkout__total">
+        <p>
+          Subtotal ({basket?.length} items: ${})
+        </p>
+      </div>
     </div>
   );
 }
