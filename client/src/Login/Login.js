8e976904da72d06f271a9ef2961e634fa12c40f8
Joey Davidson <j.davidson18@gmail.com>
2021-12-08 10:53:22 -0700

    heroku deploy init

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link, useHistory } from "react-router-dom";
+import { auth } from "../firebase";
 
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const login = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((e) => alert(e.message));
+  };
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {})
+      .catch((e) => alert(e.message));
+  };
+
   return (
     <div className="login">
       <Link to="/" className="login__link">
@@ -15,21 +34,28 @@ function Login() {
         <div className="login__container">
           <h1>Sign-In</h1>
         </div>
-        <div className="login__input">
-          <p>E-mail</p>
-          <input />
-        </div>
-        <div className="login__input">
-          <p>Password</p>
-          <input />
-        </div>
-        <button className="login__button">Sign In</button>
+        <form>
+          <div className="login__input">
+            <p>E-mail</p>
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div className="login__input">
+            <p>Password</p>
+          </div>
+          <button className="login__button" onClick={login} type="submit">
+            Sign In
+          </button>
+        </form>
         <p>
           By signing in you agree to Amazon's Conditions of Use and Sale. Please
           see our Privacy Notice, our Cookies Notice, and our Interest-Based Ads
           Notice
         </p>
-        <button className="login__buttonCreate">
+        <button onClick={register} className="login__register">
           Create your Amazon Account
         </button>
       </div>
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,20 +1,15 @@
-import firebase from 'firebase'
-
+import firebase from "firebase-tools";
 
 const firebaseApp = firebase.initializeApp({
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-    apiKey: "AIzaSyDrDiPIDdOrmc0v4ltoGoj7E9I4PUzb8AQ",
-    authDomain: "clone-c0d87.firebaseapp.com",
-    projectId: "clone-c0d87",
-    storageBucket: "clone-c0d87.appspot.com",
-    messagingSenderId: "947895704716",
-    appId: "1:947895704716:web:941f6eca11e9357e372fa9",
-    measurementId: "G-FHPXDL3ZSK"
-  };
-})
+  apiKey: "AIzaSyDrDiPIDdOrmc0v4ltoGoj7E9I4PUzb8AQ",
+  authDomain: "clone-c0d87.firebaseapp.com",
+  projectId: "clone-c0d87",
+  storageBucket: "clone-c0d87.appspot.com",
+  messagingSenderId: "947895704716",
+  appId: "1:947895704716:web:941f6eca11e9357e372fa9",
+  measurementId: "G-FHPXDL3ZSK",
+});
 
+const auth = firebase.auth();
 
-const auth = firebase.auth()
-
-export {auth};
+export { auth };
